7e3b194ea72f336e7b0429e887e1186f7772bab3
Anton Ukhanev <xedin.unknown@gmail.com>
2018-03-07 23:13:48 +0100

    Added tests for `StringTranslatorAwareTrait`
    It is also now expected to accept null

---
+++ test/unit/StringTranslatorAwareTraitTest.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace Dhii\I18n\UnitTest;
+
+use Dhii\I18n\StringTranslatorAwareTrait as TestSubject;
+
+use Dhii\I18n\StringTranslatorInterface;
+use stdClass;
+use Xpmock\TestCase;
+use Exception as RootException;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+use PHPUnit_Framework_MockObject_MockBuilder as MockBuilder;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class StringTranslatorAwareTraitTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\I18n\StringTranslatorAwareTrait';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $methods The methods to mock.
+     *
+     * @return MockObject The new instance.
+     */
+    public function createInstance($methods = [])
+    {
+        is_array($methods) && $methods = $this->mergeValues($methods, [
+            '__',
+        ]);
+
+        $mock = $this->getMockBuilder(static::TEST_SUBJECT_CLASSNAME)
+            ->setMethods($methods)
+            ->getMockForTrait();
+
+        $mock->method('__')
+                ->will($this->returnArgument(0));
+
+        return $mock;
+    }
+
+    /**
+     * Merges the values of two arrays.
+     *
+     * The resulting product will be a numeric array where the values of both inputs are present, without duplicates.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $destination The base array.
+     * @param array $source      The array with more keys.
+     *
+     * @return array The array which contains unique values
+     */
+    public function mergeValues($destination, $source)
+    {
+        return array_keys(array_merge(array_flip($destination), array_flip($source)));
+    }
+
+    /**
+     * Creates a mock that both extends a class and implements interfaces.
+     *
+     * This is particularly useful for cases where the mock is based on an
+     * internal class, such as in the case with exceptions. Helps to avoid
+     * writing hard-coded stubs.
+     *
+     * @since [*next-version*]
+     *
+     * @param string   $className      Name of the class for the mock to extend.
+     * @param string[] $interfaceNames Names of the interfaces for the mock to implement.
+     *
+     * @return MockBuilder The builder for a mock of an object that extends and implements
+     *                     the specified class and interfaces.
+     */
+    public function mockClassAndInterfaces($className, $interfaceNames = [])
+    {
+        $paddingClassName = uniqid($className);
+        $definition = vsprintf('abstract class %1$s extends %2$s implements %3$s {}', [
+            $paddingClassName,
+            $className,
+            implode(', ', $interfaceNames),
+        ]);
+        eval($definition);
+
+        return $this->getMockBuilder($paddingClassName);
+    }
+
+    /**
+     * Creates a new exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $message The exception message.
+     *
+     * @return RootException|MockObject The new exception.
+     */
+    public function createException($message = '')
+    {
+        $mock = $this->getMockBuilder('Exception')
+            ->setConstructorArgs([$message])
+            ->getMock();
+
+        return $mock;
+    }
+
+    /**
+     * Creates a translator.
+     *
+     * @param array|null $methods The methods to mock, if any.
+     *
+     * @return MockObject|StringTranslatorInterface The new translator.
+     */
+    public function createStringTranslator($methods = [])
+    {
+        is_array($methods) && $methods = $this->mergeValues($methods, [
+        ]);
+        $mock = $this->getMockBuilder('Dhii\I18n\StringTranslatorInterface')
+            ->setMethods($methods)
+            ->getMock();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInternalType(
+            'object',
+            $subject,
+            'A valid instance of the test subject could not be created.'
+        );
+    }
+
+    /**
+     * Tests that `_setTranslator()` works as expected when given `null`.
+     *
+     * @since [*next-version*]
+     */
+    public function testSetTranslatorNull()
+    {
+        $value = null;
+        $subject = $this->createInstance();
+        $_subject = $this->reflect($subject);
+
+        $_subject->translator = $this->createStringTranslator();
+        $_subject->_setTranslator($value);
+
+        $this->assertEquals($value, $_subject->translator, 'Null was not set as the new translator');
+    }
+
+    /**
+     * Tests that `_setTranslator()` works as expected.
+     *
+     * @since [*next-version*]
+     */
+    public function testSetTranslatorTranslator()
+    {
+        $value = $this->createStringTranslator();
+        $subject = $this->createInstance();
+        $_subject = $this->reflect($subject);
+
+        $_subject->translator = null;
+        $_subject->_setTranslator($value);
+
+        $this->assertSame($value, $_subject->translator, 'A translator was not set as the new translator');
+    }
+
+    /**
+     * Tests that `_setTranslator()` fails as expected when given an invalid translator.
+     *
+     * @since [*next-version*]
+     */
+    public function testSetTranslatorFailure()
+    {
+        $value = new stdClass();
+        $subject = $this->createInstance();
+        $_subject = $this->reflect($subject);
+
+        $_subject->translator = null;
+
+        $this->setExpectedException('InvalidArgumentException');
+        $_subject->_setTranslator($value);
+    }
+
+    /**
+     * Tests that `_getTranslator()` works as expected.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetTranslator()
+    {
+        $value = $this->createStringTranslator();
+        $subject = $this->createInstance();
+        $_subject = $this->reflect($subject);
+
+        $_subject->translator = $value;
+        $result = $_subject->_getTranslator();
+
+        $this->assertSame($value, $result, 'Wrong translator was retrieved');
+    }
+}
